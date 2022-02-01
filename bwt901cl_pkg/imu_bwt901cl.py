80c5e283753a9659a7a3960d332fcff0aa42d3c3
Manuel Fernandez-Carmona <manolofc@gmail.com>
2022-02-01 14:02:04 +0100

    cov

--- bwt901cl_pkg/imu_bwt901cl.py
+++ bwt901cl_pkg/imu_bwt901cl.py
@@ -17,41 +17,48 @@ class Imu901cl(Node):
         #subprocess.call("sudo chmod 777 /dev/ttyUSB0", shell=True)
         self.imu_sensor =  BWT901CL("/dev/witmotion")
 
-    def timer_callback(self):
-        msg_imu = Imu()
-        msg_mag = MagneticField()
-        msg_tmp = Temperature()
-        msg_ang = Vector3()
+        self.msg_imu = Imu()
+        self.msg_mag = MagneticField()
+        self.msg_tmp = Temperature()
+        self.msg_ang = Vector3()        
+        self.msg_imu.header.frame_id =  self.msg_mag.header.frame_id =  self.msg_tmp.header.frame_id =  'witmotion' 
 
-        msg_imu.header.frame_id =  msg_mag.header.frame_id =  msg_tmp.header.frame_id =  'witmotion' 
-        msg_imu.header.stamp =  msg_mag.header.stamp =  msg_tmp.header.stamp =  self.get_clock().now().to_msg()
+        # TODO this is a crappy hack... again
+        small_value = 0.0001
+        dummy_cov = [small_value, 0,           0, 
+                     0,           small_value, 0, 
+                     0,           0,           small_value]        
+        self.msg_imu.orientation_covariance = self.msg_imu.angular_velocity_covariance = self.msg_imu.linear_acceleration_covariance = dummy_cov
+
+    def timer_callback(self):
+        self.msg_imu.header.stamp =  self.msg_mag.header.stamp =  self.msg_tmp.header.stamp =  self.get_clock().now().to_msg()
         
         angle, angular_velocity, accel, temp, magnetic, quaternion, time = self.imu_sensor.getData()
 
-        msg_tmp.temperature = temp
-        self.pub_tmp.publish(msg_tmp)
+        self.msg_tmp.temperature = temp
+        self.pub_tmp.publish(self.msg_tmp)
 
-        msg_mag.magnetic_field.x = float(magnetic[0])
-        msg_mag.magnetic_field.y = float(magnetic[1])
-        msg_mag.magnetic_field.z = float(magnetic[2])
-        self.pub_mag.publish(msg_mag)
+        self.msg_mag.magnetic_field.x = float(magnetic[0])
+        self.msg_mag.magnetic_field.y = float(magnetic[1])
+        self.msg_mag.magnetic_field.z = float(magnetic[2])
+        self.pub_mag.publish(self.msg_mag)
 
-        msg_imu.orientation.x = quaternion[0]
-        msg_imu.orientation.y = quaternion[1]
-        msg_imu.orientation.z = quaternion[2]
-        msg_imu.orientation.w = quaternion[3]
-        msg_imu.angular_velocity.x = angular_velocity[0]
-        msg_imu.angular_velocity.y = angular_velocity[1]
-        msg_imu.angular_velocity.z = angular_velocity[2]
-        msg_imu.linear_acceleration.x = accel[0]
-        msg_imu.linear_acceleration.y = accel[1]
-        msg_imu.linear_acceleration.z = accel[2]
-        self.pub_imu.publish(msg_imu)
+        self.msg_imu.orientation.x = quaternion[0]
+        self.msg_imu.orientation.y = quaternion[1]
+        self.msg_imu.orientation.z = quaternion[2]
+        self.msg_imu.orientation.w = quaternion[3]
+        self.msg_imu.angular_velocity.x = angular_velocity[0]
+        self.msg_imu.angular_velocity.y = angular_velocity[1]
+        self.msg_imu.angular_velocity.z = angular_velocity[2]
+        self.msg_imu.linear_acceleration.x = accel[0]
+        self.msg_imu.linear_acceleration.y = accel[1]
+        self.msg_imu.linear_acceleration.z = accel[2]
+        self.pub_imu.publish(self.msg_imu)
 
-        msg_ang.x = angle[0]
-        msg_ang.y = angle[1]
-        msg_ang.z = angle[2]
-        self.pub_ang.publish(msg_ang)
+        self.msg_ang.x = angle[0]
+        self.msg_ang.y = angle[1]
+        self.msg_ang.z = angle[2]
+        self.pub_ang.publish(self.msg_ang)
 
         #print("Time:", time)
         #print("th:", angle)
